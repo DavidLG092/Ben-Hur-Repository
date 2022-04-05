e6aa9c3cfd8029296ba6d1758094a116ea32b1f4
DavidLG092 <davi91002@hotmail.com>
2022-04-05 14:44:30 -0300

    Update Menu Script
    The menu is "fully" functional! I mean, all  the sprites are set up, but some buttons don't do anything yet. But, still, it's a victory! :smile:

--- Ben-Hur/Scripts/Menu.gd
+++ Ben-Hur/Scripts/Menu.gd
@@ -11,6 +11,10 @@ const pos2 = Vector2(-1.5, 5)
 const pos3 = Vector2(23.5, 5)
 
 var m_line_scale_x
+var c_line_scale_x
+
+const c_pos1 = Vector2(-9, 5)
+const c_pos2 = Vector2(10.7, 5)
 
 # Menu variables
 var bag
@@ -25,12 +29,14 @@ func _ready():
 	
 	# Setting up sprites
 	$Sprite_Menu_Line.position = pos1
+	$Sprite_Confirmation_Line.position = c_pos1
 	
 	$Sprite_Confirmation.hide()
 	$Sprite_Confirmation_Line.hide()
 		
 	# Normal line scale
 	m_line_scale_x = $Sprite_Menu_Line.scale.x
+	c_line_scale_x = $Sprite_Confirmation_Line.scale.x
 	
 	# Setting up variables
 	bag = false
@@ -61,10 +67,18 @@ func _ready():
 	$Label_Exit.hide()
 	
 	$Label_Sure.text = "ARE YOU SURE?"
+	$Label_Sure.rect_scale = Vector2(0.15, 0.15)
+	$Label_Sure.rect_position = Vector2(-15, -7)
 	$Label_Sure.hide()
+	
 	$Label_Yes.text = "YES"
+	$Label_Yes.rect_scale = Vector2(0.25, 0.25)
+	$Label_Yes.rect_position = Vector2(-15, -1)
 	$Label_Yes.hide()
+	
 	$Label_No.text = "NO"
+	$Label_No.rect_scale = Vector2(0.25, 0.25)
+	$Label_No.rect_position = Vector2(6.5, -1)
 	$Label_No.hide()
 
 
@@ -79,6 +93,7 @@ func _process(delta):
 		$Label_Map.show()
 		$Label_Exit.show()
 		
+		# Moves confirmation line
 		if bag == false and map == false and exit == false:
 			if Input.is_action_just_pressed("move_left"):
 				match $Sprite_Menu_Line.position:
@@ -117,26 +132,52 @@ func _process(delta):
 		elif map == true:
 			$Map.show()
 		elif exit == true:
-			$AnimatedSprite.animation = "confirmation"
+			# Shows sprites and labels
+			$Sprite_Confirmation.show()
+			$Sprite_Confirmation_Line.show()
+			
+			$Label_Sure.show()
+			$Label_Yes.show()
+			$Label_No.show()
+			
+			# Moves confirmation line
 			if Input.is_action_just_pressed("move_left"):
-				if $AnimatedSprite.frame == 0:
-					$AnimatedSprite.frame = 1
-				else:
-					$AnimatedSprite.frame = 0
+				match $Sprite_Confirmation_Line.position:
+					c_pos1:
+						$Sprite_Confirmation_Line.position = c_pos2
+						$Sprite_Confirmation_Line.scale.x = 0.8
+					c_pos2:
+						$Sprite_Confirmation_Line.position = c_pos1
+						$Sprite_Confirmation_Line.scale.x = c_line_scale_x
 			if Input.is_action_just_pressed("move_right"):
-				if $AnimatedSprite.frame == 1:
-					$AnimatedSprite.frame = 0
-				else:
-					$AnimatedSprite.frame = 1
+				match $Sprite_Confirmation_Line.position:
+					c_pos1:
+						$Sprite_Confirmation_Line.position = c_pos2
+						$Sprite_Confirmation_Line.scale.x = 0.8
+					c_pos2:
+						$Sprite_Confirmation_Line.position = c_pos1
+						$Sprite_Confirmation_Line.scale.x = c_line_scale_x
 			if Input.is_action_just_pressed("attack_confirm"):
-				match $AnimatedSprite.frame:
-					0:
-						$AnimatedSprite.animation = "menu"
-						exit = false
-					1:
+				match $Sprite_Confirmation_Line.position:
+					c_pos1:
 						pass
+					c_pos2:
+						$Sprite_Confirmation.hide()
+						$Sprite_Confirmation_Line.hide()
+						
+						$Label_Sure.hide()
+						$Label_Yes.hide()
+						$Label_No.hide()
+						
+						exit = false
 			if Input.is_action_just_pressed("spare_reject"):
-				$AnimatedSprite.animation = "menu"
+				$Sprite_Confirmation.hide()
+				$Sprite_Confirmation_Line.hide()
+				
+				$Label_Sure.hide()
+				$Label_Yes.hide()
+				$Label_No.hide()
+				
 				exit = false
 	else:
 		hide()

6ed77149a35b7e2d2c34ec6217373a9e4380aa07
DavidLG092 <davi91002@hotmail.com>
2022-04-13 12:37:20 -0300

    Updated Item Script
    I was thinking about using files to handle items, but now items are fully handled and defined by their own script (for example, now there's a "type_guide" dictionary variable)!

--- Ben-Hur/Scripts/Item.gd
+++ Ben-Hur/Scripts/Item.gd
@@ -5,19 +5,36 @@ extends Object
 # var a = 2
 # var b = "text"
 
+# Guide variables
+var type_guide
+
 # Item variables
 var name
 var type
 var strength # Named "strength" but can also be used to determine item effectiveness
 
 # Called when the node enters the scene tree for the first time.
-#func _ready():
-#	pass # Replace with function body.
-
+func _ready():
+	type_guide = {
+		1: "Armor",
+		2: "Weapon",
+		3: "Heal",
+		4: "Misc",
+	}
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+# Item functions
+func create_item(nm, tp, st):
+	name = nm
+	
+	if tp in type_guide:
+		type = tp
+		
+	if (st >= 0 and st <= 100) and st % 10 == 0:
+		strength = st
 
 func set_name(val):
 	name = val
@@ -27,14 +44,16 @@ func get_name():
 
 
 func set_type(val):
-	type = val
+	if val in type_guide:
+		type = val
 
 func get_type():
 	return type
 
 
 func set_strength(val):
-	strength = val
+	if (val >= 0 and val <= 100) and val % 10 == 0:
+		strength = val
 
 func get_strength():
 	return strength
